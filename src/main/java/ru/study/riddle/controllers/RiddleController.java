d0b8503093e3cb5a35ed45bb192bbc45bf411155
Dmitriy <dadinoff1@gmail.com>
2023-11-12 15:30:35 +0300

    add nationality

--- src/main/java/ru/study/riddle/controllers/RiddleController.java
+++ src/main/java/ru/study/riddle/controllers/RiddleController.java
@@ -5,23 +5,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.study.riddle.models.Color;
 import ru.study.riddle.models.ColorRepository;
+import ru.study.riddle.models.Nationality;
+import ru.study.riddle.models.NationalityRepository;
 
 @Controller
 @RequestMapping("/title")
 public class RiddleController {
 
 
-    private String colorRightAnswer = "ответ отсутствует";
+    private String colorRightAnswer = "цвета не выбраны";
+    private String nationalityRightAnswer = "национальности не выбраны";
     ColorRepository colorRepository = new ColorRepository();
+    NationalityRepository nationalityRepository = new NationalityRepository();
 
 
     @GetMapping()
     public String riddlePage(Model model,
-                            @ModelAttribute("color") Color color){
+                             @ModelAttribute("color") Color color, @ModelAttribute("nationality")Nationality nationality){
         model.addAttribute("colors", colorRepository.getListOfColors());
-        if (colorRightAnswer.equals("54321")){
+        model.addAttribute("nationalities", nationalityRepository.getListOfNationality());
+        if (colorRightAnswer.equals("14523") && nationalityRightAnswer.equals("32154")){
             model.addAttribute("message", "Вы разгадали загадку!");
-        } else if (colorRightAnswer.equals("ответ отсутствует")){
+        } else if (colorRightAnswer.equals("цвета не выбраны") && nationalityRightAnswer.equals("национальности не выбраны")){
             model.addAttribute("message", "Разгадай загадку!");
         } else {
             model.addAttribute("message", "Попробуйте еще");
@@ -32,11 +37,17 @@ public class RiddleController {
 
 
     @PatchMapping("/add")
-    public String check(@ModelAttribute("color") Color color){
+    public String check(@ModelAttribute("color") Color color, @ModelAttribute("nationality") Nationality nationality){
+
 
         colorRightAnswer = String.valueOf(color.getId()) + color.getId2()+
                 color.getId3() + color.getId4() + color.getId5();
+
+        nationalityRightAnswer = String.valueOf(nationality.getId()) + nationality.getId2()+
+                nationality.getId3() + nationality.getId4() + nationality.getId5();
+
         System.out.println(colorRightAnswer);
+        System.out.println(nationalityRightAnswer);
 
         return "redirect:/title";
     }
--- src/main/java/ru/study/riddle/models/ColorRepository.java
+++ src/main/java/ru/study/riddle/models/ColorRepository.java
@@ -12,11 +12,12 @@ public class ColorRepository {
     public ColorRepository() {
         listOfColors = new ArrayList<>();
         int id = 0;
-        listOfColors.add(new Color(++id, 0,0,0,0, "Красный"));
-        listOfColors.add(new Color(++id,0,0,0,0,"Зеленый"));
-        listOfColors.add(new Color(++id,0,0,0,0,"Синий"));
-        listOfColors.add(new Color(++id,0,0,0,0,"Желтый"));
-        listOfColors.add(new Color(++id,0,0,0,0,"Белый"));
+        listOfColors.add(new Color(id++, 0,0,0,0, "----------------"));
+        listOfColors.add(new Color(id++, 0,0,0,0, "Красный"));
+        listOfColors.add(new Color(id++,0,0,0,0,"Зеленый"));
+        listOfColors.add(new Color(id++,0,0,0,0,"Синий"));
+        listOfColors.add(new Color(id++,0,0,0,0,"Желтый"));
+        listOfColors.add(new Color(id,0,0,0,0,"Белый"));
     }
 
     public List<Color> getListOfColors() {
